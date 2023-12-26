e9c07943e6cd991ea831e8004486556b9036e25d
medofox123 <135126620+medofox123@users.noreply.github.com>
2023-12-26 22:34:31 +0200

    Add files via upload

---
+++ games.sh
@@ -0,0 +1,159 @@
+clear
+
+n=1
+while  (($n < 3))
+do
+clear
+
+echo "                               ..'',,,''..                                 " | lolcat -s 100000
+echo "                       .:okKKKKMMMMMMMMMMMWXOdc.                           " | lolcat -s 100000
+echo "                     .lOWMMMMMMMNKMMMMMMMMMMMMMMMMWk,'                     " | lolcat -s 100000
+echo "                   .dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWOMMWx'                 " | lolcat -s 100000
+echo "                  0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMMMMMK:               " | lolcat -s 100000
+echo "                .0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX,            " | lolcat -s 100000
+echo "               lWMMMMMWXK0kkkkk0KNMMMMMMMMMMMMMMMMMMMMMNX0Okkkk;           " | lolcat -s 100000
+ echo "             OMMWkllloxO00KKK00kdollOMMMMMMMMMMMMXdllodxO00KKK0Oxo;.      " | lolcat -s 100000
+ echo "            0MK:cOWMMMMMMMMMMMMMX:.  .OMMMMMMMMx;oKMMMMMMMMMMMMMMk,.      " | lolcat -s 100000
+ echo "           kMx,NMMMMMMMMMMMMMMMX       XMMMMMM,oMMMMMMMMMMMMMMMMo         " | lolcat -s 100000
+ echo "          ;MM'kMMMMMMWXOxdoodxOk       NMMMMMN.MMMMMMMN0kdooddkOl         " | lolcat -s 100000
+ echo "          KMMk,ddollook0XNWNXX0OxdlclxNMMMMMMM;cddlllodkKXWWNXK0kxl       " | lolcat -s 100000
+ echo "         .MMMMWXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNXWMMMMMMMMMMMMMMMM.       " | lolcat -s 100000
+ echo "         cMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM,      " | lolcat -s 100000
+ echo "         oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNWMMMNWXMKMM,      " | lolcat -s 100000
+ echo "         lMMMMMMMMMM0MK0KKMMMMMMMMMMMKNMMMMMMN:l:KMMMMX.XMMMoWkMkMM       " | lolcat -s 100000
+ echo "         ,MMMMMMMMMM00MxMxMMMMMMMMMMMK;xMMMMk.OMX OMWx,KMMMMMMMMMM0.      " | lolcat -s 100000
+ echo "          NMMMMMMMMMMMMMMMMMMMMMMMMMMMMOllold,OMM.dodOMMMMMMMMMMMM'       " | lolcat -s 100000
+echo "           lMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:kMM.NMMMMMMMMMMMMMMd        " | lolcat -s 100000
+echo "            OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMckMM.KMMMMMMMMMMMMMd         " | lolcat -s 100000
+echo "             kMMMMMMMMMMMMMMMMMMMMMMMMMMMMXXNNokKK:0NNXMMMMMMMMW:          " | lolcat -s 100000
+ echo "             :WMMMMMMMMMMMMMMMMMMMMMMMMMx''''''''.''''dMMMMMM0            " | lolcat -s 100000
+echo "                :XMMMMMMMMMMMMMMMMMMMMMMO''''ckd'...''''dMMWk              " | lolcat -s 100000
+echo "                   .kXWMMMMMMMMMMMMMMMMMd'''lOxl.. ..''':X0k;              " | lolcat -s 100000
+echo "                        .cMMMMMMMMWNXKXNx''dKl'' ..ld''.'dOOkdl'           " | lolcat -s 100000
+ echo "                        XMXkollooodxxxdl.'lx'....cxk,' NMMMMMMM0.         " | lolcat -s 100000
+  echo "                      OMXx0NMMMMMMMMMMMN.'...';dOoc;'.cWMMMMMMMW,        " | lolcat -s 100000
+ echo "                      lMMMMMMMMMMMMMMMMMO.'.';lxo.c,''',ccxXMMMMMo.       " | lolcat -s 100000
+echo "                      ,MMMMMMMMMMMMMMMWk;.''',ld;..'',''cMW0dllllc.        " | lolcat -s 100000
+ echo "                    .WMMMMMMMMMWXOdlllOo';x;00;. .';00,cMMMMMMMX          " | lolcat -s 100000
+echo "                     OMMMMMMMxoodx0XMMMMo':''k:....':Ox,;MMMMMMMK.         " | lolcat -s 100000
+echo "                    ,MMMMMMMMMMMMMMMMMMMl''''......'c0o',WMMMMMMW          " | lolcat -s 100000
+echo "                    kMMMMMMMMMMMMMMMMMMMc''........';dk',WMMMMMMM,         " | lolcat -s 100000
+echo "                    NMMMMMMMMMMMMMMMMMMMd'''.'.''''''''''NMMMMMMMx         " | lolcat -s 100000
+echo "                                          ':cc:;,;:c::,                    " | lolcat -s 100000
+echo "------------------------------------------------------" | lolcat
+echo "-----------[DEV:Abdelhamedmohammed-MEDOFOX]-----------" | lolcat -as 50
+echo "------------------------------------------------------" | lolcat 
+
+
+echo "[1]  Moon-buggy " | lolcat -as 1000 
+echo "[2]  Tetris " | lolcat -as 1000
+echo "[3]  Pacman " | lolcat -as 1000
+echo "[4]  Space- Invaders " | lolcat -as 1000
+echo "[5]  Snake-Game " | lolcat -as 1000
+echo "[6]  Greed " | lolcat -as 1000
+echo "[7]  Nethack " | lolcat -as 1000
+echo "[8]  Hangman " | lolcat -as 1000
+echo "[9]  2048 " | lolcat -as 1000
+echo "[10] Sudoku " | lolcat -as 1000
+echo "[0]  Enable Arrow Keys " | lolcat -as 1000
+echo "[a]  About me " | lolcat -as 1000
+echo "[e]  EXIT "  | lolcat -as 1000
+
+
+
+
+
+# Select a Choice :
+
+echo -e "Enter a Choice : \c" | lolcat
+read option
+
+case $option in 
+	"1") 
+	echo "Press q on your Keyboard to Exit this game " | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	moon-buggy
+;;
+	"2")
+	echo "Press CTRL + c to Exit this game " | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	bastet
+;;
+	"3")
+	echo "Press q on your Keyboard to Exit this game " | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	pacman
+	sleep 2
+;;
+	"4") 
+	echo "Press q on your Keyboard to Exit this game " | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	ninvaders || nInvaders 
+;;
+	"5")
+	echo "Press q on your Keyboard to Exit this game " | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	nsnake
+	sleep 2
+;;
+	"6")
+	echo "Press q on your Keyboard and then y to Exit this game...If this game crash you can Exit the session(its a bug in this game.) " | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	greed
+;;
+	"7")
+	echo "You can press CTRL + c and then give the answer of asked Question to Quit the Games." | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	nethack
+;;
+	"8") cd HangmanPy && python hangman.py
+	echo "You can Play the game and when you win or lose the game you will be asked to Quit the game or Replay." | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+;;
+	"9")
+	echo "Press q and then y to exit the game." | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	./2048
+;;
+	"10")
+	echo "Press Capital Q to exit the game." | lolcat -a
+	read -p "Press [ENTER] to Start the Game: " | lolcat -a
+	nudoku
+;;
+	"e") 
+	echo "▄▄▄▄·  ▄· ▄▌▄▄▄ .      " | lolcat -s 10000
+	echo "▐█ ▀█▪▐█▪██▌▀▄.▀·      " | lolcat -s 10000
+	echo "▐█▀▀█▄▐█▌▐█▪▐▀▀▪▄      " | lolcat -s 10000
+	echo "██▄▪▐█ ▐█▀·.▐█▄▄▌      " | lolcat -s 10000
+	echo "·▀▀▀▀   ▀ •  ▀▀▀  ▀  ▀ " | lolcat -s 10000
+	break
+;;
+	"a")
+	clear
+	echo "𝐀𝐛𝐨𝐮𝐭 𝐦𝐞: " | lolcat
+	echo "  ___  " | lolcat -as 100
+	echo " |[_]| " | lolcat -as 100
+	echo " |+ ;|  Hello " | lolcat -as 100
+	echo " '---' " | lolcat | lolcat -as 100
+	echo "[-----------------------------------------------------]" | lolcat -as 100
+	echo " [ɴᴀᴍᴇ] = [ᗪ乇ᐯ:爪乇ᗪㄖ¿千ㄖ乂] " | lolcat -as 50
+	echo ""
+	echo " [ᴡᴇʙsɪᴛᴇ] = [https://solo.to/medofox] " | lolcat -as 50
+	echo ""
+	echo " [ɢɪᴛʜᴜʙ] = [https://github.com/medofox123] " | lolcat -as 50
+	echo ""
+	echo "[ɪɴsᴛᴀɢʀᴀᴍ]" = [www.instagram.com/x_mr.fox_x/] | lolcat -as 50
+	echo "[-----------------------------------------------------]" | lolcat -as 100
+	read -p "Press [ENTER] to continue: "
+
+;;
+	"0")
+	mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings
+	echo "Please Restart to Auto-adjust the Keys"
+	read -p "Press [ENTER] to continue or you can Restart the Terminal(its recommended):"
+;;
+	*)
+	echo "Invalid Choice !!" | lolcat
+	read -p "Press [ENTER] to continue: "
+esac
+done
